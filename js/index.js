a4eb861121b0a780a4b52bc1128b40e45b91da3f
sharad-epam <47317812+sharad-epam@users.noreply.github.com>
2019-04-03 15:36:01 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
-"use strict";
-
 const API_KEY = `acfe6a939a5642678d8ea20b50748780`;
 const channel = "bbc-news";
-const URL = `https://newsapi.org/v1/articles?source=${channel}&apiKey=${API_KEY}`;
+const url = `https://newsapi.org/v1/articles?source=${channel}&apiKey=${API_KEY}`;
 let resultHtml = "";
 const getResults = (
   { author, title, description, publishedAt, url, urlToImage },
@@ -12,43 +10,15 @@ const getResults = (
   resultHtml += `${item}<div class="title">${title}</div><div class="desc">${description}</div><div class="publish">${publishedAt}</div><div class="img"><a href="${url}" target="_blank"><img class="pic" src=${urlToImage} /></a></div>`;
 };
 const fetchingNews = async url => {
-  const res = await fetch(url);
+  try {
+    const res = await fetch(url);
+  } catch (err) {
+    console.error(err);
+  }
   let data = await res.json();
   let newsId = document.getElementById("news");
   data = data.articles.map((item, index) => getResults(item, index));
   newsId.insertAdjacentHTML("afterbegin", resultHtml);
 };
 
-fetchingNews(URL);
-// let returnHtml = "";
-// const getResults = (
-//   { author, title, description, publishedAt, url, urlToImage },
-//   index
-// ) => {
-//   const singleVal = index === 0 ? `<h1 class="author">${author}</h1>` : "";
-//   returnHtml += `${singleVal}<div class="title">${title}</div><div class="desc">${description}</div><div class="publish">${publishedAt}</div><div class="img"><a href="${url}" target="_blank"><img class="pic" src=${urlToImage} /></a></div>`;
-// };
-// class fetchdata {
-//   FetchingNews(url) {
-//     let data = {};
-//     fetch(url)
-//       .then(res => res.json())
-//       .then(item => {
-//         data = item;
-//         let elementId = document.getElementById("news");
-//         data.articles.forEach((item, index) => getResults(item, index));
-//         elementId.insertAdjacentHTML("afterbegin", returnHtml);
-//       })
-//       .catch(error => {
-//         console.error("Something went wrong on server", error);
-//       });
-//   }
-// }
-// const instance = new fetchdata();
-// export default instance;
-
-// import instance from "./index.js";
-// const API_KEY = `acfe6a939a5642678d8ea20b50748780`;
-// const channel = "bbc-news";
-// const URL = `https://newsapi.org/v1/articles?source=${channel}&apiKey=${API_KEY}`;
-// instance.FetchingNews(URL);
+fetchingNews(url);
