8369308311e2b09a4b521080ee32fa67dc02ab7f
sharad-epam <47317812+sharad-epam@users.noreply.github.com>
2019-04-05 13:47:39 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ const getResults = (
 ) => {
   const item =
     index === 0 ? `<result-info author=${author} ></result-info>` : "";
-  resultHtml += `<result-info item=${item} title=${title} desc=${description} publish=${publishedAt} pic=${urlToImage}></result-info>`;
+  resultHtml += `<result-info item=${item} title=${title} desc=${description} publish=${publishedAt} pic=${urlToImage}>${urlToImage}</result-info>`;
 };
 class fetchItems {
   fetchingNews = async url => {
