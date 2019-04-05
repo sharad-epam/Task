e09ba1c71fdae3bddd868561ce1a004f30772065
sharad-epam <47317812+sharad-epam@users.noreply.github.com>
2019-04-05 11:03:19 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
 let resultHtml = "";
-
+const getResults = (
+  { author, title, description, publishedAt, url, urlToImage },
+  index
+) => {
+  const item = index === 0 ?<result-info [author]=`${item}` ></result-info> : "";
+  resultHtml += `${item}<div class="title">${title}</div><div class="desc">${description}</div><div class="publish">${publishedAt}</div><div class="img"><a href="${url}" target="_blank"><img class="pic" src=${urlToImage} /></a></div>`;
+};
 class fetchItems {
   fetchingNews = async url => {
     try {
       const res = await fetch(url);
       let data = await res.json();
       let newsId = document.getElementById("news");
-      data = data.articles.map((item, index) =>
-        returnUI.getResults(item, index)
-      );
+      data = data.articles.map((item, index) => getResults(item, index));
       newsId.insertAdjacentHTML("afterbegin", resultHtml);
     } catch (err) {
       console.error(err);
@@ -16,15 +20,6 @@ class fetchItems {
   };
 }
 
-class returnHtml extends fetchItems {
-  getResults = (
-    { author, title, description, publishedAt, url, urlToImage },
-    index
-  ) => {
-    const item = index === 0 ? `<h1 class="author">${author}</h1>` : "";
-    resultHtml += `${item}<div class="title">${title}</div><div class="desc">${description}</div><div class="publish">${publishedAt}</div><div class="img"><a href="${url}" target="_blank"><img class="pic" src=${urlToImage} /></a></div>`;
-  };
-}
 const result = new fetchItems();
-const returnUI = new returnHtml();
 export default result;
+
