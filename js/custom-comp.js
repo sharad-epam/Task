3353ea6f45d3eafd7238b2efacd96d579b2e9620
sharad-epam <47317812+sharad-epam@users.noreply.github.com>
2019-04-05 11:25:43 +0530

    Update custom-comp.js

--- js/custom-comp.js
+++ js/custom-comp.js
@@ -2,20 +2,23 @@ class ResultInfo extends HTMLElement {
   constructor() {
     super();
     const shadow = this.attachShadow({ mode: "open" });
+    const wrapper = document.createElement("div");
 
     const author = document.createElement("h1");
     author.setAttribute("class", "author");
-    let authordata = this.getAttribute("author");
-    author.textContent = authordata;
+    author.textContent = this.getAttribute("author");
 
     const title = document.createElement("div");
     title.setAttribute("class", "title");
+    title.textContent = this.getAttribute("title");
 
     const desc = document.createElement("div");
     desc.setAttribute("class", "desc");
+    desc.textContent = this.getAttribute("desc");
 
     const publish = document.createElement("div");
     publish.setAttribute("class", "publish");
+    publish.textContent = this.getAttribute("publish");
 
     const img = document.createElement("div");
     img.setAttribute("class", "img");
@@ -23,16 +26,18 @@ class ResultInfo extends HTMLElement {
     let imgUrl;
     this.hasAttribute("img") ? (imgUrl = this.getAttribute("img")) : "No Image";
 
-    let image = document.createElement("img");
-    image.src = imgUrl;
-    img.appendChild(image);
+    let pic = document.createElement("img");
+    publish.setAttribute("class", "pic");
+    pic.src = imgUrl;
+    img.appendChild(pic);
 
-    shadow.appendChild(author);
+    shadow.appendChild(wrapper);
+    wrapper.appendChild(author);
     wrapper.appendChild(title);
     wrapper.appendChild(desc);
     wrapper.appendChild(publish);
     wrapper.appendChild(img);
-    wrapper.appendChild(image);
+    wrapper.appendChild(pic);
   }
 }
 
