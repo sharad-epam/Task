b2451475ca9492bd86c13bb4614d1870439cd6b5
sharad-epam <47317812+sharad-epam@users.noreply.github.com>
2019-04-05 19:06:36 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
-// let resultHtml = "";
-// const getResults = (
-//   { author, title, description, publishedAt, url, urlToImage },
-//   index
-// ) => {
-//   console.log(title);
-//   const item = index === 0 ? `<result-info ${author} >` : "";
-//   resultHtml += `<div><result-info title="${title}" description="${description}" publishedAt="${publishedAt}" img="${urlToImage}"></result-info></div>`;
-// };
-// class fetchItems {
-//   fetchingNews = async url => {
-//     try {
-//       const res = await fetch(url);
-//       let data = await res.json();
-//       let newsId = document.getElementById("news");
-//       data.articles.map((item, index) => getResults(item, index));
-//       newsId.insertAdjacentHTML("afterend", resultHtml);
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
-// }
+let resultHtml = "";
+const getResults = (
+  { author, title, description, publishedAt, url, urlToImage },
+  index
+) => {
+  console.log(title);
+  const item = index === 0 ? `<result-info ${author} >` : "";
+  resultHtml += `<div><result-info title="${title}" description="${description}" publishedAt="${publishedAt}" img="${urlToImage}"></result-info></div>`;
+};
+class fetchItems {
+  fetchingNews = async url => {
+    try {
+      const res = await fetch(url);
+      let data = await res.json();
+      let newsId = document.getElementById("news");
+      data.articles.map((item, index) => getResults(item, index));
+      newsId.insertAdjacentHTML("afterend", resultHtml);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+}
 
-// const result = new fetchItems();
-// export default result;
+const result = new fetchItems();
+export default result;
