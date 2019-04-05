66b77724ffd1ce9063cd0c5301e09b2f49b7a9bb
sharad-epam <47317812+sharad-epam@users.noreply.github.com>
2019-04-05 14:51:57 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import result from "./index.js";
-const API_KEY = `acfe6a939a5642678d8ea20b50748780`;
+const API_KEY = `720c0314e8b2423eb7e1ffca5a1eeeb1`;
 const channel = "bbc-news";
 const URL = `https://newsapi.org/v1/articles?source=${channel}&apiKey=${API_KEY}`;
 result.fetchingNews(URL);
